a5ac6994d0d9d8978f371a70b78d643357211862
irenebell <irinaselen1@gmail.com>
2022-04-21 15:33:20 +0300

    fizzbuzz

--- lesson_1.py
+++ lesson_1.py
@@ -1,5 +1,24 @@
-def addition(a, b):
-    result = a + b
-    print(result)
+def fizzbuzz(n):
+    new_array = []
+    for i in range(1, n + 1):
+        if (i % 3 == 0) and (i % 5 == 0):
+            print("FizzBuzz")
+            new_array.append("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+            new_array.append("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+            new_array.append("Buzz")
+        else:
+            print(i)
+            new_array.append(i)
+    print(new_array)
+    return new_array
 
-addition(10, 5)
+
+fizzbuzz(15)
+
+
+
+
